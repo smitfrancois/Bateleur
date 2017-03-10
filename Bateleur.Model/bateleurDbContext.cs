17f24f920a2e32241d582da41ecd32fec27036d9
Francois Smit <smit.fran@gmail.com>
2017-03-10 09:10:32 +0200

    Project Foundation Continued

---
+++ Bateleur.Model/bateleurDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bateleur.Model
+{
+    public class BateleurDbContext:DbContext
+    {
+        public BateleurDbContext() : base()
+        {
+            
+        }
+    }
+}
