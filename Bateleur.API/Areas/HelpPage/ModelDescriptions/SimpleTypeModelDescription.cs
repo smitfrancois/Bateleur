f71dc216460616b5e7819acd0216815b91ab2ae6
Francois Smit <smit.fran@gmail.com>
2017-03-10 09:05:37 +0200

    Comitting project foundation

---
+++ Bateleur.API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Bateleur.API.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Bateleur.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bateleur.API.WebApiApplication" Language="C#" %>
